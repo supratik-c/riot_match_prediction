a986ed9c367a2d9a560f1c6a14f340ae19136caf
supratik-c <scdipto@gmail.com>
2022-12-10 21:34:42 +0000

    Time recoring removed from query loop

--- query_loop.R
+++ query_loop.R
@@ -1,8 +1,6 @@
 for(j in 1:3000){
 
-    # Add query time
-    queryTime <- tibble() 
-    StartTime <- Sys.time()
+
     
     # Get sample match
     player_esi <- get_sample_player()
@@ -40,17 +38,9 @@ for(j in 1:3000){
         mutate(matchId = recent_matchId) %>% 
         full_join(match_data, by = "matchId")
     
-    # Time recording
-    EndTime <- Sys.time()
-    sampleTime <- difftime(EndTime, StartTime, units = "secs")
-    sampleTime <- list(
-        "phase" = "sampling",
-        "value" = sampleTime)
-    queryTime <- bind_rows(queryTime, sampleTime)
     
     
     # Ranked W/L Ratio
-    StartTime <- Sys.time()
     winlosses <- tibble()
     for(id in data_reduced$summonerId){
         print(paste("Attempting: ", id))
@@ -61,19 +51,10 @@ for(j in 1:3000){
     data_wl <- merge(data_reduced, winlosses, 
                           by = "summonerId",
                           all.x = T)
-    
-    
-    # Time recording
-    EndTime <- Sys.time()
-    sampleTime <- difftime(EndTime, StartTime, units = "secs")
-    sampleTime <- list(
-        "phase" = "winloss",
-        "value" = sampleTime)
-    queryTime <- bind_rows(queryTime, sampleTime)
+
     
     
     # Champ Mastery
-    StartTime <- Sys.time()
     champ_df <- tibble()
     champ_iterable <- tibble(
         "summonerId" = data_reduced$summonerId,
@@ -92,17 +73,9 @@ for(j in 1:3000){
                      by = "summonerId",
                      all.x = T)
     
-    
-    # Time recording
-    EndTime <- Sys.time()
-    sampleTime <- difftime(EndTime, StartTime, units = "secs")
-    sampleTime <- list(
-        "phase" = "champ_mastery",
-        "value" = sampleTime)
-    queryTime <- bind_rows(queryTime, sampleTime)
+
     
     # Streak
-    StartTime <- Sys.time()
     streaks <- tibble()
     for(id in data_reduced$puuid){
         print(paste("Attempting:", id))
@@ -114,41 +87,22 @@ for(j in 1:3000){
                           all.x = T)
     
     
-    # Time recording
-    EndTime <- Sys.time()
-    sampleTime <- difftime(EndTime, StartTime, units = "secs")
-    sampleTime <- list(
-        "phase" = "streak",
-        "value" = sampleTime)
-    queryTime <- bind_rows(queryTime, sampleTime)
-    
     
     # Match timeline data
-    StartTime <- Sys.time()
     match_timeline <- get_match_timeline(recent_matchId)
     data_final <- merge(data_streaks, match_timeline,
                         by = "puuid",
                         all.x = T) 
     
     
-    # Time recording
-    EndTime <- Sys.time()
-    sampleTime <- difftime(EndTime, StartTime, units = "secs")
-    sampleTime <- list(
-        "phase" = "match_timeline",
-        "value" = sampleTime)
-    queryTime <- bind_rows(queryTime, sampleTime)
-    
-    # Write to csv
-    write_csv(data_final, "data/match_data.csv", append = T)
-    write_csv(queryTime, "data/record_times.csv", append = T)
-    print(paste("Match written:", recent_matchId, "Record:", j))
     
     # Write to Azure
     dbWriteTable(riot_db,
                  "F_MATCHES",
                  data_final,
                  append = T)
+    
+    print(paste("Match written:", recent_matchId, "Record:", j))
 
 }
 
