3d9162dce1c26ab8aaaadf6931a6f86ddc2970bb
supratik-c <scdipto@gmail.com>
2022-12-15 19:58:10 +0000

    Modelling workflow RQ1 and 2 added

--- Functions/packages.R
+++
@@ -1,6 +0,0 @@
-library(httr)
-library(jsonlite)
-library(tidyverse)
-library(RODBC)
-library(odbc)
-library(anytime)
--- modelling/query_time_analysis.R
+++
@@ -1,6 +0,0 @@
-raw_query_timings <- read_csv("data/record_times.csv", col_names = c("phase", "value"))
-
-averages <- raw_query_timings %>% 
-    group_by(phase) %>% 
-    summarise(avg = mean(value))
-    
